8a72c6293e34a55c91b2b159292f40a9590d57d4
AleksSmith <vampire.box@mail.ru>
2021-04-07 22:51:04 +0300

    Fixed Tasks 2

--- Lesson_13/homework/main.c
+++ Lesson_13/homework/main.c
@@ -3,20 +3,20 @@
 
 #include"../sources/geek.h"
 
-
-/* 1.) Реализовать обход графа в глубину с использованием стека void depthTraversStack(int** matrix, int size) */
-
-void clearVisited(int* arr, int size) {
+void clearArr(int* arr, int size) {
     for (int i = 0; i < size; ++i) {
         arr[i] = 0;
     }
+
+/* 1.) Реализовать обход графа в глубину с использованием стека void depthTraversStack(int** matrix, int size) */
+
 }
 
 void depthTraversStack(int** matrix, int start, const int size){
     OneLinkList *stack = (OneLinkList*)malloc(sizeof(OneLinkList));
     initOneLinkList(stack);
     int visited[size];
-    clearVisited(visited, size);
+    clearArr(visited, size);
     pushOneLinkStack(stack, start);
     while(stack->size > 0){
         int indx = popOneLinkStack(stack);
@@ -40,13 +40,33 @@ void depthTraversStack(int** matrix, int start, const int size){
 a) Обход графа по матрице смежности (с подсчётом всех вершин графа) - void adjacencyCount(int** matrix, const int size)*/
 
 void adjacencyCount(int** matrix, const int size){
+    int links[size];
+    clearArr(links, size);
+    for (int  i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if (matrix[i][j] == 1)
+                links[i]++;
+        }
+    }
+    for (int i = 0; i < size; i++){
+        printf("%d-%d ", i, links[i]);
+    }
+    
+}
+
+/*
+b) 
+Обход графа функцией (с подсчётом только смежных со стартовой вершин) - void traversalCount(int** matrix, int start, const int size)
+В конце обхода вывести два получившихся списка всех узлов в порядке уменьшения количества ссылок на них. */
+
+void traversalCount(int** matrix, int start, const int size){
     OneLinkList *stack = (OneLinkList*)malloc(sizeof(OneLinkList));
     initOneLinkList(stack);
     int visited[size];
     int links[size];
-    clearVisited(visited, size);
-    clearVisited(links, size);
-    pushOneLinkStack(stack, 0);
+    clearArr(visited, size);
+    clearArr(links, size);
+    pushOneLinkStack(stack, start);
     while(stack->size > 0){
         int indx = popOneLinkStack(stack);
         if(visited[indx] == 1) continue;
@@ -59,74 +79,52 @@ void adjacencyCount(int** matrix, const int size){
                 pushOneLinkStack(stack, i);
         }
         printf("-%d ",links[indx]);
-        
     }
-
-
 }
-
-/*
-b) Обход графа функцией (с подсчётом только смежных со стартовой вершин) - void traversalCount(int** matrix, int start, const int size)
-В конце обхода вывести два получившихся списка всех узлов в порядке уменьшения количества ссылок на них. */
-
 
 
 // Результатом работы должен стать один файл с кодом на языке С, содержащий функцию main и все необходимые функции.
 
-//int matrix[n][n] = {
+//int graf[n][n] = {
 //        {0, 1, 1, 0, 0, 0},
-//        {0, 0, 0, 1, 1, 1},
+//        {1, 0, 1, 1, 0, 0},
+//        {1, 1, 0, 0, 0, 0},
+//        {0, 1, 0, 1, 0, 0},
 //        {0, 0, 0, 0, 0, 1},
-//        {1, 0, 0, 0, 0, 0},
-//        {0, 0, 0, 0, 0, 0},
 //        {0, 0, 0, 0, 1, 0},
 //};
 
 int main(int argc, char const *argv[]){
-    // 1.
-    printf("Task 1\n");
     const int size = 6;
     int** graf = init2dIntArray(graf, size, size);
     set2dInt(graf, 0, 1, 1);
     set2dInt(graf, 0, 2, 1);
+    set2dInt(graf, 1, 0, 1);
+    set2dInt(graf, 1, 2, 1);
     set2dInt(graf, 1, 3, 1);
-    set2dInt(graf, 1, 4, 1);
-    set2dInt(graf, 1, 5, 1);
-    set2dInt(graf, 2, 5, 1);
-    set2dInt(graf, 3, 0, 1);
+    set2dInt(graf, 2, 0, 1);
+    set2dInt(graf, 2, 1, 1);
+    set2dInt(graf, 3, 1, 1);
+    set2dInt(graf, 3, 3, 1);
+    set2dInt(graf, 4, 5, 1);
     set2dInt(graf, 5, 4, 1);
     print2dIntArray(graf,size,size,2);
     printf("\n");
+// 1.
+    printf("Task 1:\n");
     for (int i = 0; i < size; i++){
         depthTraversStack(graf, i, size);
         printf("\n");
     }
     printf("\n");
-    // 2.a.
-     printf("Task 2a\n");
-    int** adjacency = init2dIntArray(adjacency, size, size);
-    set2dInt(adjacency, 0, 1, 1);
-    set2dInt(adjacency, 0, 2, 1);
-    set2dInt(adjacency, 0, 3, 1);
-    set2dInt(adjacency, 1, 0, 1);
-    set2dInt(adjacency, 1, 3, 1);
-    set2dInt(adjacency, 1, 4, 1);
-    set2dInt(adjacency, 1, 5, 1);
-    set2dInt(adjacency, 2, 0, 1);
-    set2dInt(adjacency, 2, 5, 1);
-    set2dInt(adjacency, 3, 0, 1);
-    set2dInt(adjacency, 3, 1, 1);
-    set2dInt(adjacency, 4, 1, 1);
-    set2dInt(adjacency, 4, 4, 1);
-    set2dInt(adjacency, 4, 5, 1);
-    set2dInt(adjacency, 5, 1, 1);
-    set2dInt(adjacency, 5, 2, 1);
-    set2dInt(adjacency, 5, 4, 1);
-    print2dIntArray(adjacency,size,size,2);
+// 2.a.
+    printf("Task 2a:\n");
+    adjacencyCount(graf,size);
     printf("\n");
-    adjacencyCount(adjacency,size);
+// 2.b
+    printf("Task 2b:\n");
+    traversalCount(graf, 4, size);
     printf("\n");
-
     
     return 0;
 
